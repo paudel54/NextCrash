44c10e920f5a70da9cb6b9f692ea61b405258528
paudel54 <2sansrit@gmail.com>
2023-05-21 18:25:43 +0545

    Completed Next JS fundamentals

--- next-fundamentals/app/note.js
+++ next-fundamentals/app/note.js
@@ -7,3 +7,18 @@
 
 
 // You can't have page.js and route.js on same level . route.js is used esp of creating an APis
+
+// Two way of imporving SEOs
+// 1.Static MetaData 2. Dynamic Metadata
+
+export const metadata = {
+    title: 'Home',
+}
+// or
+export async function generatemetadata({ params, searchParams }) {
+    const product = await getProduct(params.id);
+    return { title: product.title }
+}
+
+// Output:
+//<head> <title> Unique Product </title> </head>
