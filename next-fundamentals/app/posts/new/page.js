7769f95d4c4279d44d598bc1911cd4ef6493756e
paudel54 <2sansrit@gmail.com>
2023-05-21 17:39:06 +0545

    Added Nested routing with folder Organizations

---
+++ next-fundamentals/app/posts/new/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// posts/new/page.js
+const page = () => {
+    return (
+        <div>
+            New Post
+        </div>
+    )
+}
+
+export default page
---
+++ next-fundamentals/app/posts/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+//posts/page.js
+const page = () => {
+    return (
+        <div>
+            This Section Consists of Post
+        </div>
+    )
+}
+
+export default page
